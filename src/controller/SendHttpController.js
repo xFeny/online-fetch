65e0af97dbc16513b47da113d040680279488db7
Feny <1220301855@qq.com>
2024-10-11 18:50:40 +0800

    默认值

--- src/controller/SendHttpController.js
+++ src/controller/SendHttpController.js
@@ -1,35 +1,42 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 // 发送http请求
 router.post("/httpRequest", (req, res) => {
+  // 获取POST请求体中的数据
+  let { url, method, headers, params, contentType } = req.body;
 
-    // 获取POST请求体中的数据
-    let { url, method, headers, params, contentType } = req.body;
+  if (url) {
+    res.json("url 不能为空");
+    return;
+  }
+  method = method ? method : "GET";
+  headers = headers ? JSON.parse(headers) : {};
+  params = params ? JSON.parse(params) : {};
 
-    headers = headers ? JSON.parse(headers) : {}
-    params = params ? JSON.parse(params) : {}
-
-    axios({
-        url,
-        method,
-        headers: {
-            'Content-Type': contentType,
-            ...headers
-        },
-        data: params,
-        params: method === 'GET' ? new URLSearchParams(params) : null,
-    }).then(function (response) {
-        res.json(response.data)
-    }).catch(function (error) {
-        console.log(error.message)
-        if (error.response) {
-            res.json(error.response.data)
-        } else {
-            let { status, message } = error
-            res.json({ code: status, message })
-        }
+  axios({
+    url,
+    method,
+    headers: {
+      "Content-Type":
+        contentType || "application/x-www-form-urlencoded;charset=UTF-8",
+      ...headers,
+    },
+    data: params,
+    params: method === "GET" ? new URLSearchParams(params) : null,
+  })
+    .then(function (response) {
+      res.json(response.data);
     })
+    .catch(function (error) {
+      console.log(error.message);
+      if (error.response) {
+        res.json(error.response.data);
+      } else {
+        let { status, message } = error;
+        res.json({ code: status, message });
+      }
+    });
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
 const express = require("express");
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // 使用body-parser中间件解析JSON类型的请求体
 app.use(bodyParser.json());
 // 静态资源目录
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
-const parentDir = path.resolve(__dirname, '..');
+const parentDir = path.resolve(__dirname, "..");
 app.get("/", (req, res) => {
-    res.sendFile(parentDir + '/public/example.html')
+  res.sendFile(parentDir + "/public/example.html");
 });
-
 
 // 自动加载路由模块
 const loadRoutes = (dir) => {
-    const routesDir = path.join(__dirname, dir);
-    const routeFiles = fs.readdirSync(routesDir);
-    routeFiles.forEach((file) => {
-        if (file.endsWith('.js')) {
-            const routeModule = require(path.join(routesDir, file));
-            console.log(file)
-            app.use(routeModule);
-        }
-    });
+  const routesDir = path.join(__dirname, dir);
+  const routeFiles = fs.readdirSync(routesDir);
+  routeFiles.forEach((file) => {
+    if (file.endsWith(".js")) {
+      const routeModule = require(path.join(routesDir, file));
+      console.log(file);
+      app.use(routeModule);
+    }
+  });
 };
-loadRoutes('controller');
+loadRoutes("controller");
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
